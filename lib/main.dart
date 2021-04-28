5781232419ab496a65b2f38228e0386384bd4d3d
Utsharga Rozario <utshargarozario@gmail.com>
2021-04-27 20:40:57 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:expenses_app/widgets/user_transaction.dart';
+import 'package:flutter/material.dart';
+
+import './widgets/user_transaction.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expenses App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  // String titleInput;
+  // String amountInput;
+
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expenses App'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue[100],
+                child: Text('Chart!'),
+                elevation: 5,
+              ),
+            ),
+            UserTransactions(),
+          ],
+        ),
+      ),
+    );
+  }
+}
